f8bbc1a3415cc5df6406bab4f40eb5955403c57c
elKuston <romolocaponera@uma.es>
2023-01-24 15:49:53 +0100

    Creado TipoPieza
    El TipoPieza contiene información común a todas las piezas del mismo tipo (rango de conectores, path de la imagen, etc).

--- src/main/java/modelica/ModelicaGenerator.java
+++ src/main/java/modelica/ModelicaGenerator.java
@@ -6,21 +6,24 @@ import modelo.Conector;
 import modelo.Conexion;
 import modelo.Pieza;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringJoiner;
 
 public class ModelicaGenerator {
-    public static final String IMPORT_BASIC = "B";
-    public static final String IMPORT_LOGIC = "L";
-    public static final String IMPORT_DIGITAL = "D";
-    public static final String IMPORT_SOURCES = "S";
-    private static final String DIGITAL = "Modelica.Electrical.Digital";
-    private static final String BASIC = "Modelica.Electrical.Digital.Basic";
-    private static final String LOGIC = "Modelica.Electrical.Digital.Interfaces.Logic";
-    private static final String SOURCES = "Modelica.Electrical.Digital.Sources";
+    public static final String BASIC = "B";
+    public static final String LOGIC = "L";
+    public static final String DIGITAL = "D";
+    public static final String SOURCES = "S";
+    private static final String DIGITAL_IMPORT = "Modelica.Electrical.Digital";
+    private static final String BASIC_IMPORT = "Modelica.Electrical.Digital.Basic";
+    private static final String LOGIC_IMPORT = "Modelica.Electrical.Digital.Interfaces.Logic";
+    private static final String SOURCES_IMPORT = "Modelica.Electrical.Digital.Sources";
 
     public static String generarCodigoModelica(Circuito circuito) {
         StringBuilder codigoModelica = new StringBuilder();
         codigoModelica.append("model ").append(modelName(circuito)).append("\n\t");
+        codigoModelica.append(imports()).append("\n\t");
         codigoModelica.append(declaraciones(circuito));
         codigoModelica.append("equation\n\t").append(connect(circuito));
         codigoModelica.append("end ").append(modelName(circuito)).append(";");
@@ -28,26 +31,46 @@ public class ModelicaGenerator {
         return codigoModelica.toString();
     }
 
+    private static String imports() {
+        StringJoiner sj = new StringJoiner(";\n\t");
+        sj.add("import " + DIGITAL + " = " + DIGITAL_IMPORT);
+        sj.add("import " + BASIC + " = " + BASIC_IMPORT);
+        sj.add("import " + LOGIC + " = " + LOGIC_IMPORT);
+        sj.add("import " + SOURCES + " = " + SOURCES_IMPORT);
+        sj.add("\n");
+        return sj.toString();
+    }
+
     private static String declaraciones(Circuito circuito) {
         StringJoiner sj = new StringJoiner(";\n\t");
-
-        sj.add("import " + IMPORT_DIGITAL + " = " + DIGITAL);
-        sj.add("import " + IMPORT_BASIC + " = " + BASIC);
-        sj.add("import " + IMPORT_LOGIC + " = " + LOGIC);
-        sj.add("import " + IMPORT_SOURCES + " = " + SOURCES);
         for (Pieza p : circuito.getComponentes()) {
-            if (p.getClaseModelica().startsWith(IMPORT_SOURCES + ".")) {
-                sj.add("parameter " + IMPORT_LOGIC + " " + nombreFuente(p) + " = " + IMPORT_LOGIC +
-                        ".'0'");
-                sj.add(p.getClaseModelica() + " " + nombrePieza(p) + "(x=" + nombreFuente(p) + ")");
-            } else {
-                sj.add(p.getClaseModelica() + " " + nombrePieza(p) + "( n=" +
-                        nConectoresEntrada(p) + ")");
+            List<String> lineasDeclaracion = generarDeclaracionPieza(p);
+            for (String s : lineasDeclaracion) {
+                sj.add(s);
             }
         }
 
         sj.add("\n");
         return sj.toString();
+    }
+
+    private static List<String> generarDeclaracionPieza(Pieza p) {
+        List<String> declaracion = new ArrayList<>();
+        switch (p.getTipoPieza()) {
+
+            case AND, OR -> {
+                declaracion.add(
+                        p.getTipoPieza().getClaseModelica() + " " + nombrePieza(p) + "( n=" +
+                                nConectoresEntrada(p) + ")");
+            }
+            case SET -> {
+                declaracion.add(
+                        "parameter " + LOGIC + " " + nombreFuente(p) + " = " + LOGIC + ".'0'");
+                declaracion.add(p.getTipoPieza().getClaseModelica() + " " + nombrePieza(p) + "(x=" +
+                        nombreFuente(p) + ")");
+            }
+        }
+        return declaracion;
     }
 
     private static String nombreFuente(Pieza p) {
@@ -61,7 +84,7 @@ public class ModelicaGenerator {
 
     private static String nombrePieza(Pieza p) {
         String nombre;
-        String clase = p.getClaseModelica().split("\\.")[1];
+        String clase = p.getTipoPieza().getClaseModelica().split("\\.")[1];
         if (p.getIdPieza() == null) {
             nombre = "p_" + clase + "_" + p.hashCode();
         } else {
@@ -72,10 +95,20 @@ public class ModelicaGenerator {
 
     private static String connect(Circuito circuito) {
         StringJoiner sj = new StringJoiner(";\n\t");
-        for (int i = 0; i < circuito.getConexiones().size(); i++) {
-            Conexion c = circuito.getConexiones().get(i);
-            sj.add("connect(" + nombreConector(c.getOrigen()) + ", " +
-                    nombreConector(c.getDestino()) + "[" + (i + 1) + "])");
+        for (Conexion c : circuito.getConexiones()) {
+            Conector salida =
+                    c.getOrigen().getTipoConector().equals(TipoConector.SALIDA) ? c.getOrigen() :
+                            c.getDestino();
+            Conector entrada =
+                    c.getOrigen().getTipoConector().equals(TipoConector.ENTRADA) ? c.getOrigen() :
+                            c.getDestino();
+            //Posicion del conector de entrada dentro de la pieza
+            int indiceEntrada = entrada.getPieza().getConectores().stream()
+                                       .filter(con -> con.getTipoConector()
+                                                         .equals(TipoConector.ENTRADA)).toList()
+                                       .indexOf(entrada) + 1;
+            sj.add(String.format("connect(%s,%s[%d])", nombreConector(salida),
+                    nombreConector(entrada), indiceEntrada));
         }
         sj.add("\n");
         return sj.toString();
