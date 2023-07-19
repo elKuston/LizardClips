f318d4507c8c20bd431c5e5fed3b5f1ca3563183
elKuston <romolocaponera@uma.es>
2023-07-19 19:53:06 +0200

    Añadidos componentes del paquete Gates

--- src/main/java/caponera/uned/tfm/lizardclips/constant/TabPaleta.java
+++ src/main/java/caponera/uned/tfm/lizardclips/constant/TabPaleta.java
@@ -3,7 +3,7 @@ package caponera.uned.tfm.lizardclips.constant;
 import lombok.Getter;
 
 public enum TabPaleta {
-    BASIC("Basic"), SOURCES("Sources");
+    BASIC("Basic"), SOURCES("Sources"), Gates("Gates");
     @Getter
     private final String nombre;
 
